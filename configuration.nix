bd6b989fa7d3245d6ed988f2402a1abd141868aa
fahdfady <fahd.fady212@gmail.com>
2025-02-04 11:23:36 +0200

    batman

---
+++ configuration.nix
@@ -0,0 +1,170 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs,lib, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Africa/Cairo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ar_EG.UTF-8";
+    LC_IDENTIFICATION = "ar_EG.UTF-8";
+    LC_MEASUREMENT = "ar_EG.UTF-8";
+    LC_MONETARY = "ar_EG.UTF-8";
+    LC_NAME = "ar_EG.UTF-8";
+    LC_NUMERIC = "ar_EG.UTF-8";
+    LC_PAPER = "ar_EG.UTF-8";
+    LC_TELEPHONE = "ar_EG.UTF-8";
+    LC_TIME = "ar_EG.UTF-8";
+  };
+
+  services.flatpak.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+services.xserver = {
+# Enable the GNOME Desktop Environment.
+displayManager.gdm.enable = true;
+desktopManager.gnome.enable = true;
+
+  videoDrivers = [ "amdgpu" ];  # Ensure we're using the right driver
+screenSection = ''
+  Option "ModeValidation" "AllowNonEdidModes"
+  ModeLine "1440x900_60.00"  106.50  1440 1528 1672 1904  900 903 909 934 -hsync +vsync
+'';
+};
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+## second Monitor does not report its supported resolutions properly. Force EDID data:
+# hardware.opengl.extraPackages = with pkgs; [ vaapiVdpau ];
+#services.xserver.monitorSection = ''
+ # Section "Monitor"
+  #  Identifier "VGA-1"
+   # Option "CustomEDID" "VGA-1:/path/to/edid.bin"  # Get EDID from Windows/another OS
+  #EndSection
+# '';
+
+hardware.firmware = with pkgs; [
+    firmwareLinuxNonfree # Includes AMD-specific firmware
+  ];
+  
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+ enable = true;
+    alsa.enable = true;    # ALSA integration
+    pulse.enable = true;   # PulseAudio compatibility
+    jack.enable = true;    # JACK support (for pro-audio)
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.fahd = {
+    isNormalUser = true;
+    description = "fahd";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "fahd";
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+ nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+             "vscode"
+             "jetbrains.clion"
+           ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  discord 
+  appimage-run 
+  git
+  vscode
+  arandr
+#  jetbrains.clion
+  ];
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
